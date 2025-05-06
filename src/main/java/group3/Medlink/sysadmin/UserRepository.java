b0dbdbb36cb63cdd57f83097f2086d667a8317bb
yajaira120 <yalonso120@gmail.com>
2025-05-06 19:37:49 -0400

    Uploading current work

--- src/main/java/group3/Medlink/sysadmin/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package group3.Medlink.sysadmin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByRole(String role);
-}
