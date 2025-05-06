ae55bd73b04acae5d3a99130e16ff05e847e5051
yajaira120 <yalonso120@gmail.com>
2025-05-06 19:37:49 -0400

    Uploading current work

--- src/main/java/group3/Medlink/reply/ReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package group3.Medlink.reply;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReplyRepository extends JpaRepository<Reply,Integer> {
-}
